a66f0fb9805cef5e7f8ff1d83d36315d3d42d9f7
Al <audelise.h@gmail.com>
2017-10-08 13:31:28 -0400

    Styling the page with bootstrap.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $.ajax({
     url: apiUrl,
     success: function (res) {
         $("#title").text(res.title);
+        $("#date").text(res.date);
         $("#hdurl").attr("src", res.hdurl);
         $("#explanation").text(res.explanation);
     }
